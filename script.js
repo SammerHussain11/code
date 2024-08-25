643029c1c993d382a002b0573c5ff641a5868089
SammerHussain11 <154684975+SammerHussain11@users.noreply.github.com>
2024-08-25 07:56:04 +0500

    github code push

---
+++ script.js
@@ -0,0 +1,12 @@
+// // Callbacks, async, promise
+
+// const Display = (sum) => sum;
+
+// const Calculator = (num1, num2, Display) => {
+//   let sum = num1 + num2;
+//   console.log(Display(sum));
+// };
+
+// Calculator(24, 26, Display);
+
+// 
